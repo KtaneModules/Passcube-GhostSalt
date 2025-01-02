82ea9ef4e0490a8ce20dccdec4681cf71ad5d1e0
QuinnWuest <qwuest123@gmail.com>
2025-01-02 11:55:42 -0500

    quick remodel

--- Assets/PasscubeScript.cs
+++ Assets/PasscubeScript.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
-using Passcube;
 
 public class PasscubeScript : MonoBehaviour
 {
@@ -14,20 +13,28 @@ public class PasscubeScript : MonoBehaviour
     public KMAudio Audio;
     public GameObject Cube;
     public KMSelectable[] ArrowSels;
-    public KMSelectable EnterSel;
-    public TextMesh[] Text;
+    public KMSelectable[] InputSels;
+    public TextMesh[] CubeText;
 
     private int _moduleId;
     private static int _moduleIdCounter = 1;
     private bool _moduleSolved;
 
+    private Coroutine[] _buttonAnims = new Coroutine[6];
+
     private void Start()
     {
         _moduleId = _moduleIdCounter++;
         for (int i = 0; i < 4; i++)
+        {
             ArrowSels[i].OnInteract += ArrowPress(i);
-        EnterSel.OnInteract += EnterPress;
-        EnterSel.OnInteractEnded += EnterRelease;
+            ArrowSels[i].OnInteractEnded += ArrowRelease(i);
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            InputSels[i].OnInteract += InputPress(i);
+            InputSels[i].OnInteractEnded += InputRelease(i);
+        }
     }
 
     private KMSelectable.OnInteractHandler ArrowPress(int i)
@@ -36,51 +43,64 @@ public class PasscubeScript : MonoBehaviour
         {
             if (_moduleSolved)
                 return false;
-
+            if (_buttonAnims[i] != null)
+                StopCoroutine(_buttonAnims[i]);
+            _buttonAnims[i] = StartCoroutine(ButtonAnimation(ArrowSels[i].gameObject, true));
             return false;
         };
     }
 
-    private bool EnterPress()
+    private Action ArrowRelease(int i)
     {
-        if (_moduleSolved)
-            return false;
-        
-        return false;
-    }
-    
-    private void EnterRelease()
-    {
-        if (_moduleSolved)
+        return delegate ()
+        {
+            if (_moduleSolved)
+                return;
+            if (_buttonAnims[i] != null)
+                StopCoroutine(_buttonAnims[i]);
+            _buttonAnims[i] = StartCoroutine(ButtonAnimation(ArrowSels[i].gameObject, false));
             return;
-
-        return;
+        };
     }
 
-    private IEnumerator ButtonAnimation (GameObject obj, bool isPress)
+    private KMSelectable.OnInteractHandler InputPress(int i)
     {
-        var duration = 0.2f;
+        return delegate ()
+        {
+            if (_moduleSolved)
+                return false;
+            if (_buttonAnims[i + 4] != null)
+                StopCoroutine(_buttonAnims[i + 4]);
+            _buttonAnims[i + 4] = StartCoroutine(ButtonAnimation(InputSels[i].gameObject, true));
+            return false;
+        };
+    }
+
+    private Action InputRelease(int i)
+    {
+        return delegate ()
+        {
+            if (_moduleSolved)
+                return;
+            if (_buttonAnims[i + 4] != null)
+                StopCoroutine(_buttonAnims[i + 4]);
+            _buttonAnims[i + 4] = StartCoroutine(ButtonAnimation(InputSels[i].gameObject, false));
+            return;
+        };
+    }
+
+    private IEnumerator ButtonAnimation(GameObject obj, bool isPress)
+    {
+        var duration = 0.075f;
         var elapsed = 0f;
-        var posY = obj.transform.localPosition.y;
-        var goal = isPress ? 0 : 1;
+        var pos = obj.transform.localPosition;
+        var goal = isPress ? 0.0135f : 0.0185f;
         while (elapsed < duration)
         {
-
+            obj.transform.localPosition = new Vector3(pos.x, Easing.InOutQuad(elapsed, pos.y, goal, duration), pos.z);
             yield return null;
             elapsed += Time.deltaTime;
         }
-    }
-
-    private IEnumerator TextJitter()
-    {
-        while (true)
-        {
-            for (int i = 0; i < 6; i++)
-                Text[i].color -= new Color32(0, 0, 0, 150);
-            yield return null;
-            for (int i = 0; i < 6; i++)
-                Text[i].color += new Color32(0, 0, 0, 150);
-            yield return null;
-        }
+        obj.transform.localPosition = new Vector3(pos.x, goal, pos.z);
     }
 }
