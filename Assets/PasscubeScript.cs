8d871e00332eb323af0ea3fe439a7e44a09ac9b3
QuinnWuest <qwuest123@gmail.com>
2025-01-02 11:13:53 -0500

    cleanup

--- Assets/PasscubeScript.cs
+++ Assets/PasscubeScript.cs
@@ -7,118 +7,67 @@ using KModkit;
 using Rnd = UnityEngine.Random;
 using Passcube;
 
-public class PasscubeScript : MonoBehaviour {
-
-    static int _moduleIdCounter = 1;
-    int _moduleID = 0;
-
+public class PasscubeScript : MonoBehaviour
+{
     public KMBombModule Module;
     public KMBombInfo Bomb;
     public KMAudio Audio;
     public GameObject Cube;
-    public KMSelectable[] Arrows;
-    public KMSelectable Enter;
+    public KMSelectable[] ArrowSels;
+    public KMSelectable EnterSel;
     public TextMesh[] Text;
 
-    private int[] PasswordLetterPositions = { 0, 1, 2, 3, 4, 5 };
-    private int[] FaceIndexes = { 0, 1, 2, 3, 4, 5 };
-    private string Password;
-    private string Entry;
-    private bool Pressing;
+    private int _moduleId;
+    private static int _moduleIdCounter = 1;
+    private bool _moduleSolved;
 
-    void Awake()
+    private void Start()
     {
-        _moduleID = _moduleIdCounter++;
+        _moduleId = _moduleIdCounter++;
         for (int i = 0; i < 4; i++)
-        {
-            int x = i;
-            Arrows[i].OnInteract += delegate { StartCoroutine(ArrowPress(x)); return false; };
-        }
-        Enter.OnInteract += delegate { StartCoroutine(EnterPress()); return false; };
-        Password = Words.WordList[Rnd.Range(0, Words.WordList.Length)];
-        PasswordLetterPositions.Shuffle();
-        for (int i = 0; i < 6; i++)
-            Text[i].text = Password[PasswordLetterPositions[i]].ToString();
-        Debug.Log(Password);
-        StartCoroutine(TextJitter());
+            ArrowSels[i].OnInteract += ArrowPress(i);
+        EnterSel.OnInteract += EnterPress;
+        EnterSel.OnInteractEnded += EnterRelease;
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private IEnumerator EnterPress()
+    private KMSelectable.OnInteractHandler ArrowPress(int i)
     {
-        for (int i = 0; i < 3; i++)
+        return delegate ()
         {
-            Enter.transform.localPosition -= new Vector3(0, 0.002f, 0);
-            yield return null;
-        }
-        for (int i = 0; i < 3; i++)
-        {
-            Enter.transform.localPosition += new Vector3(0, 0.002f, 0);
-            yield return null;
-        }
+            if (_moduleSolved)
+                return false;
+
+            return false;
+        };
     }
 
-    private IEnumerator ArrowPress(int pos)
+    private bool EnterPress()
     {
-        if (!Pressing)
+        if (_moduleSolved)
+            return false;
+        
+        return false;
+    }
+    
+    private void EnterRelease()
+    {
+        if (_moduleSolved)
+            return;
+
+        return;
+    }
+
+    private IEnumerator ButtonAnimation (GameObject obj, bool isPress)
+    {
+        var duration = 0.2f;
+        var elapsed = 0f;
+        var posY = obj.transform.localPosition.y;
+        var goal = isPress ? 0 : 1;
+        while (elapsed < duration)
         {
-            Pressing = true;
-            int[] TempIndexes = FaceIndexes;
-            for (int i = 0; i < 3; i++)
-            {
-                Arrows[pos].transform.localPosition -= new Vector3(0, 0.002f, 0);
-                yield return null;
-            }
-            switch (pos)
-            {
-                case 0:
-                    for (int i = 0; i < 15; i++)
-                    {
-                        Cube.transform.localRotation = Quaternion.Euler(6, 0, 0) * Cube.transform.localRotation;
-                        yield return null;
-                    }
-                    FaceIndexes = new int[] { TempIndexes[1], TempIndexes[5], TempIndexes[2], TempIndexes[0], TempIndexes[4], TempIndexes[3] };
-                    break;
-                case 1:
-                    for (int i = 0; i < 15; i++)
-                    {
-                        Cube.transform.localRotation = Quaternion.Euler(0, 0, -6) * Cube.transform.localRotation;
-                        yield return null;
-                    }
-                    FaceIndexes = new int[] { TempIndexes[2], TempIndexes[1], TempIndexes[5], TempIndexes[3], TempIndexes[0], TempIndexes[4] };
-                    break;
-                case 2:
-                    for (int i = 0; i < 15; i++)
-                    {
-                        Cube.transform.localRotation = Quaternion.Euler(-6, 0, 0) * Cube.transform.localRotation;
-                        yield return null;
-                    }
-                    FaceIndexes = new int[] { TempIndexes[3], TempIndexes[0], TempIndexes[2], TempIndexes[5], TempIndexes[4], TempIndexes[1] };
-                    break;
-                default:
-                    for (int i = 0; i < 15; i++)
-                    {
-                        Cube.transform.localRotation = Quaternion.Euler(0, 0, 6) * Cube.transform.localRotation;
-                        yield return null;
-                    }
-                    FaceIndexes = new int[]{ TempIndexes[4], TempIndexes[1], TempIndexes[0], TempIndexes[3], TempIndexes[5], TempIndexes[2] };
-                    break;
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                Arrows[pos].transform.localPosition += new Vector3(0, 0.002f, 0);
-                yield return null;
-            }
-            Pressing = false;
+
+            yield return null;
+            elapsed += Time.deltaTime;
         }
     }
 
